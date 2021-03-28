d6222965b78ed61282e94c6287bbf77db007850f
Ahmed sultan <en.ahmedsultan@gmail.com>
2021-03-28 19:44:44 +0200

    upload on git

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TestFrameWork",
-            url: "https://justshare.vodafone.com/messages/QHqaPiZY8TEKX8TL52lXGM/attachments/5DvQHSnH9C2GEMzsreuht7/download/TestFrameWrok-0.0.1.xcframework.zip",
+            url: "https://github.com/engsulta/TestFramework/raw/main/xcframeworks/TestFrameWrok-0.0.1.xcframework.zip",
             checksum: "ea3f9d2ea83b7c10c4c118b8ac1558080e562339971fa5aee034d4b94a03d4de"
         ),
     ]
