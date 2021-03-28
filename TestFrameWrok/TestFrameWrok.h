0c057827a54a7bfed1a4fa6aceeb1a398549c768
Ahmed sultan <en.ahmedsultan@gmail.com>
2021-03-28 18:26:23 +0200

    Initial Commit

---
+++ TestFrameWrok/TestFrameWrok.h
@@ -0,0 +1,18 @@
+//
+//  TestFrameWrok.h
+//  TestFrameWrok
+//
+//  Created by Ahmed Sultan on 28/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFrameWrok.
+FOUNDATION_EXPORT double TestFrameWrokVersionNumber;
+
+//! Project version string for TestFrameWrok.
+FOUNDATION_EXPORT const unsigned char TestFrameWrokVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameWrok/PublicHeader.h>
+
+
