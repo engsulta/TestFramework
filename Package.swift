71edfd0c6bee00aa3435fe2e897ed87441b7f1a4
Ahmed sultan <en.ahmedsultan@gmail.com>
2021-03-28 20:05:45 +0200

    change checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "TestFrameWork",
             url: "https://github.com/engsulta/TestFramework/raw/main/xcframeworks/TestFrameWrok-0.0.1.xcframework.zip",
-            checksum: "ea3f9d2ea83b7c10c4c118b8ac1558080e562339971fa5aee034d4b94a03d4de"
+            checksum: "2735947065d7277f9cb5b21a6815af76a4fa2f2245ce9960f289640f579996d6"
         ),
     ]
 )
